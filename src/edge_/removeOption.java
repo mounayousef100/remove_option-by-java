060eb7c739f5a23b73ecaecfa7411c631be8040c
mounayousef100 <118472640+mounayousef100@users.noreply.github.com>
2022-11-22 10:07:12 +0000

    delet

--- src/edge_/removeOption.java
+++ src/edge_/removeOption.java
@@ -16,23 +16,29 @@ public class removeOption {
 		driver.get("http://127.0.0.1:5501/index.html"
 				+ "");
 		 List <WebElement> theStudents = driver.findElements(By.tagName("option"));
-		 int theTotalNumberOfStudents = theStudents.size();
-	      System.out.println(theTotalNumberOfStudents+ " this is the orginal numper");
-	    		     int howManyItims = 4 ;
-	     System.out.println(howManyItims+ " this is remove number of items i");
-		    for (int i= 0 ;i<howManyItims;i++) {
+//		 int theTotalNumberOfStudents = theStudents.size();
+//	      System.out.println(theTotalNumberOfStudents+ " this is the orginal numper");
+//	    		     int howManyItims = 4 ;
+//	     System.out.println(howManyItims+ " this is remove number of items i");
+		    for (int i= 0 ;i<theStudents.size();i++) {
 				
+		    	if (i%2 == 0) {
+		    		theStudents.get(i).click();
+		    		System.err.println("sorry i will remove  "+theStudents.get(i).getText() );
 	 driver.findElement(By.xpath("//*[@id=\"remove\"]")).click();
-		
+		    	}
+		    	else {
+		    		System.out.println("Sorry i will keep  "+ theStudents.get(i).getText());
+		    	}
 			}
 		   
-		    List <WebElement> theStudantAfterRemove= driver.findElements(By.tagName("option"));
-		   int actualNumber = theStudantAfterRemove.size();
-		   System.out.println(actualNumber+ " this is atualNumber");
-		   int expectedItems= (theTotalNumberOfStudents - howManyItims);
-		   System.out.println(expectedItems+ " this is expectedItems");
-			Assert.assertEquals(theStudantAfterRemove,expectedItems);
-			
+//		    List <WebElement> theStudantAfterRemove= driver.findElements(By.tagName("option"));
+//		   int actualNumber = theStudantAfterRemove.size();
+//		   System.out.println(actualNumber+ " this is atualNumber");
+//		   int expectedItems= (theTotalNumberOfStudents - howManyItims);
+//		   System.out.println(expectedItems+ " this is expectedItems");
+//			Assert.assertEquals(theStudantAfterRemove,expectedItems);
+//			
 	
 	}
 
